17f9e29ad9746c0c7003c2a0874df00cfda8a029
Kacper Paczos <kacper-paczos@linux.pl>
2023-10-31 23:43:42 +0100

    Added getDependencyFromConda_flatpakVersion.py

---
+++ getDependencyFromConda_flatpakVersion.py
@@ -0,0 +1,59 @@
+import json
+import subprocess
+import hashlib
+import requests
+
+print("Initiating process: Creation of conda environment")
+subprocess.run(['conda', 'create', '--name', 'temp', '--dry-run', '--json', 'jupyterlab', '-c', 'conda-forge'], stdout=open('dependencies.json', 'w'))
+with open('dependencies.json', 'r') as file:
+    print("Initiating process: Loading JSON file")
+    data = json.load(file)
+    links = data['actions']['LINK']
+
+install = []
+mamba_install = []
+sources = []
+for link in links:
+    print("Initiating process: Processing each link")
+    name, version = link['name'], link['version']
+    install.append(f'install -Dm 755 -t $FLATPAK_DEST/download {name}.conda')
+    mamba_install.append(f'$FLATPAK_DEST/anaconda/bin/mamba install $FLATPAK_DEST/download/{name}.conda')
+    url = f'https://anaconda.org/conda-forge/{name}/{version}/download/linux-64/{name}-{version}.conda'
+    print(f"Initiating process: Downloading file from URL: {url}")
+    try:
+        
+        response = requests.get(url)
+        sha256_hash = hashlib.sha256(response.content).hexdigest()
+        sources.append({
+            '- type': 'file',
+            'only-arches': ' x86_64',
+            'url': url,
+            'sha256': sha256_hash,
+            'dest-filename': f'{name}.conda'
+        })
+    except:
+        print("Failure occurred. Exception handling initiated. Unable to calculate checksum.")
+        sources.append({
+            '- type': 'file',
+            'only-arches': ' x86_64',
+            'url': url,
+            'sha256': 'idontknow',
+            'dest-filename': f'{name}.conda'
+        })
+
+print("===========================================")
+print("Initiating process: Displaying all sources")
+print("===========================================")
+for i in install:
+    print(i)
+print("-------------------------------------------")
+for i in mamba_install:
+    print(i)
+print("-------------------------------------------")
+for s in sources:
+    print(f"- type: {s['- type']}")
+    print(f"  only-arches:\n    -{s['only-arches']}")
+    print(f"  url: {s['url']}")
+    print(f"  sha256: {s['sha256']}")
+    print(f"  dest-filename: {s['dest-filename']}")
+print("--------------------------------------------")
